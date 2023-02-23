9e89a3799e3976cbcd0b32e3e8d2f980f5440839
Leonardo Collado Torres <lcolladotor@gmail.com>
2023-02-23 17:14:16 -0600

    Update script to our use case

--- code/01_transfer_data/01_psomagen_data.sh
+++ code/01_transfer_data/01_psomagen_data.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 #$ -cwd
-#$ -l mem_free=10G,h_vmem=10G,h_fsize=300G
+#$ -l mem_free=2G,h_vmem=2G,h_fsize=300G
 #$ -N psomagen_data
-#$ -o logs/psomagen_data.txt
-#$ -e logs/psomagen_data.txt
+#$ -o logs/psomagen_data.$TASK_ID.txt
+#$ -e logs/psomagen_data.$TASK_ID.txt
 #$ -m e
+#$ -t 1-2
+#$ -tc 2
 
 echo "**** Job starts ****"
 date
@@ -19,8 +21,11 @@ echo "Task id: ${SGE_TASK_ID}"
 ## List current modules for reproducibility
 module list
 
+## Create output directory
+mkdir -p /dcs04/lieber/marmaypag/fentanylRat_LIBD4205/fentanyl_rat_hb_amy/raw-data/FASTQ/psomagen
+
 ## Locate directory
-ORIGINALDIR=$(awk "NR==${SGE_TASK_ID}" qSVA_dcl01_dirs.txt)
+ORIGINALDIR=$(awk "NR==${SGE_TASK_ID}" 01_psomagen_directories.txt)
 echo "Processing sample ${ORIGINALDIR}"
 date
 
@@ -33,9 +38,11 @@ du -sk --apparent-size ${ORIGINALDIR}/ | awk '{$1=$1/(1024^3); print $1, "TB";}'
 ## List owners of all the files in the original path
 find ${ORIGINALDIR}/ -exec ls -l {} \; | grep -v total | tr -s ' ' | cut -d ' ' -f3,9-
 
-## Copy from dcl01 to dcs04
-rsync -rltgvh --chown=:lieber_lcolladotor ${ORIGINALDIR}/ /dcs04/lieber/lcolladotor/qSVA_LIBD3080/${BASEDIR}/
+## Copy from /dcs04/lieber/data/transfers/psomagen to dcs04/marmaypag
+rsync -rltgvh --chown=:lieber_lcolladotor ${ORIGINALDIR}/ /dcs04/lieber/marmaypag/fentanylRat_LIBD4205/fentanyl_rat_hb_amy/raw-data/FASTQ/psomagen/${BASEDIR}/
 
+## Update permissions
+sh /dcs04/lieber/lcolladotor/_jhpce_org_LIBD001/update_permissions_spatialteam.sh /dcs04/lieber/marmaypag/fentanylRat_LIBD4205/fentanyl_rat_hb_amy/raw-data/FASTQ/psomagen
 
 echo "**** Job ends ****"
 date
