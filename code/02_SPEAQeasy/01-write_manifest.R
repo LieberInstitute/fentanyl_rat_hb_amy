0b222ad080c83f701ae2af0a2b6f4813c9d50582
Nick-Eagles <nick.eagles@libd.org>
2023-03-07 17:07:16 -0500

    Re-create the manifest for SPEAQeasy, making sure to include the E-36 sample

--- code/02_SPEAQeasy/01-write_manifest.R
+++ code/02_SPEAQeasy/01-write_manifest.R
@@ -8,40 +8,34 @@ man_path = here('processed-data', '02_SPEAQeasy', 'samples.manifest')
 sample_info_path = here(
     'raw-data', 'FentanylvsSaline_SelfAdministration_RNAextraction.xlsx'
 )
+fastq_dir = here('raw-data', 'FASTQ', 'psomagen')
 
-get_fastq_paths = function(read_num) {
-    return(
-        c(
-            list.files(
-                here('raw-data', 'FASTQ', 'psomagen', 'AN00012355'),
-                pattern = paste0('.*_', read_num, '\\.fastq\\.gz$'),
-                full.names = TRUE
-            ),
-            list.files(
-                here('raw-data', 'FASTQ', 'psomagen', 'AN00012356'),
-                pattern = paste0('.*_', read_num, '\\.fastq\\.gz$'),
-                full.names = TRUE
-            )
-        )
-    )
-}
+r1 = list.files(
+    fastq_dir, pattern = paste0('.*_1\\.fastq\\.gz$'), full.names = TRUE,
+    recursive = TRUE
+)
+
+r2 = list.files(
+    fastq_dir, pattern = paste0('.*_2\\.fastq\\.gz$'), full.names = TRUE,
+    recursive = TRUE
+)
 
 sample_info = read_excel(
     sample_info_path, sheet = "SubjectInformation for Analysis"
-)
+) |>
+    filter(!is.na(!!as.symbol('Sample No.')))
 
 #   Use underscores in sample IDs instead of hyphens or spaces
 sample_info['Tissue Punch Label'] = gsub(
     ' |-', '_', sample_info[['Tissue Punch Label']]
 )
 
-r1 = get_fastq_paths(1)
 sample_ids = sample_info[
-        match(ss(basename(r1), '_'), sample_info[['Sample No.']]),
-    ] |>
+    match(ss(basename(r1), '_'), sample_info[['Sample No.']]),
+] |>
     pull('Tissue Punch Label')
-    
-man = paste(r1, 0, get_fastq_paths(2), 0, sample_ids, sep = '\t')
+
+man = paste(r1, 0, r2, 0, sample_ids, sep = '\t')
 
 writeLines(man, con = man_path)
 
