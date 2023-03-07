d527946d8f5457b31adc81d27f53f72d2bc2bf0a
Nick-Eagles <nick.eagles@libd.org>
2023-03-07 14:02:18 -0500

    Use the sample-info sheet to give informative and up-to-date sample IDs for the FASTQs

--- code/02_SPEAQeasy/01-write_manifest.R
+++ code/02_SPEAQeasy/01-write_manifest.R
@@ -1,8 +1,13 @@
 library('here')
 library('jaffelab')
 library('sessioninfo')
+library('readxl')
+library('tidyverse')
 
 man_path = here('processed-data', '02_SPEAQeasy', 'samples.manifest')
+sample_info_path = here(
+    'raw-data', 'FentanylvsSaline_SelfAdministration_RNAextraction.xlsx'
+)
 
 get_fastq_paths = function(read_num) {
     return(
@@ -21,8 +26,21 @@ get_fastq_paths = function(read_num) {
     )
 }
 
+sample_info = read_excel(
+    sample_info_path, sheet = "SubjectInformation for Analysis"
+)
+
+#   Use underscores in sample IDs instead of hyphens or spaces
+sample_info['Tissue Punch Label'] = gsub(
+    ' |-', '_', sample_info[['Tissue Punch Label']]
+)
+
 r1 = get_fastq_paths(1)
-sample_ids = ss(basename(r1), '_')
+sample_ids = sample_info[
+        match(ss(basename(r1), '_'), sample_info[['Sample No.']]),
+    ] |>
+    pull('Tissue Punch Label')
+    
 man = paste(r1, 0, get_fastq_paths(2), 0, sample_ids, sep = '\t')
 
 writeLines(man, con = man_path)
