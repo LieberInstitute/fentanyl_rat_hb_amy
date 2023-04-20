9b3efe0c820aaa821d3c0b0953c031ebe65d5d4d
daianna21 <daianna2121@outlook.es>
2023-04-19 19:27:12 -0600

    Merge data in colData(RSE)

--- code/03_EDA/01_QCA.R
+++ code/03_EDA/01_QCA.R
@@ -18,6 +18,9 @@ load(here('raw-data/count_objects/rse_jx_Jlab_experiment_n33.Rdata'), verbose=TR
 sample_data <- as.data.frame(read_excel("raw-data/FentanylvsSaline_SelfAdministration_RNAextraction.xlsx"))
 
 
+
+## 1.1 Data exploration and preparation
+
 ## Verify sample data is the same in gene, exon and jx datasets
 identical(colData(rse_gene), colData(rse_exon))
 # [1] TRUE
@@ -29,9 +32,9 @@ identical(colData(rse_gene), colData(rse_jx))
 
 ## Correct sample ID in sample data
 sample_data$SAMPLE_ID <- str_replace_all(sample_data$`Tissue Punch Label`, c(" "="_", "-"="_"))
-
 ## Discard unused samples
 sample_data <- sample_data[which(sample_data$SAMPLE_ID %in% rse_gene$SAMPLE_ID),]
-
+## Merge data in colData
+colData(rse_gene) <- merge(colData(rse_gene), sample_data, by='SAMPLE_ID')
 
 
