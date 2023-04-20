e1b700f2147b3c4fb70e36de1393fe67fc6c7bde
daianna21 <daianna2121@outlook.es>
2023-04-19 19:21:01 -0600

    Correct SAMPLE_ID in sample data

--- code/03_EDA/01_QCA.R
+++ code/03_EDA/01_QCA.R
@@ -2,6 +2,7 @@
 library(here)
 library(SummarizedExperiment)
 library(readxl)
+library(stringr)
 
 #######################   Exploratory Data Analysis   #######################
 
@@ -13,7 +14,6 @@ library(readxl)
 load(here('raw-data/count_objects/rse_gene_Jlab_experiment_n33.Rdata'), verbose=TRUE)
 load(here('raw-data/count_objects/rse_exon_Jlab_experiment_n33.Rdata'), verbose=TRUE)
 load(here('raw-data/count_objects/rse_jx_Jlab_experiment_n33.Rdata'), verbose=TRUE)
-
 ## Load sample data
 sample_data <- as.data.frame(read_excel("raw-data/FentanylvsSaline_SelfAdministration_RNAextraction.xlsx"))
 
@@ -24,6 +24,14 @@ identical(colData(rse_gene), colData(rse_exon))
 identical(colData(rse_gene), colData(rse_jx))
 # [1] TRUE
 
+
+## Add sample data to colData of gene RSE
+
+## Correct sample ID in sample data
+sample_data$SAMPLE_ID <- str_replace_all(sample_data$`Tissue Punch Label`, c(" "="_", "-"="_"))
+
+## Discard unused samples
+sample_data <- sample_data[which(sample_data$SAMPLE_ID %in% rse_gene$SAMPLE_ID),]
 
 
 
