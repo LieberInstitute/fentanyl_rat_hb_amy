ce745b83ac83af8dd05df412264f78b36e57dbe4
daianna21 <daianna2121@outlook.es>
2023-04-19 18:40:41 -0600

    Verify sample data is the same in gene, exon and jx datasets

--- code/03_EDA/01_QCA.R
+++ code/03_EDA/01_QCA.R
@@ -3,6 +3,11 @@ library(here)
 library(SummarizedExperiment)
 library(readxl)
 
+#######################   Exploratory Data Analysis   #######################
+
+
+##  1. Quality Control Analysis
+
 
 ## Load RSE object at gene level
 load(here('raw-data/count_objects/rse_gene_Jlab_experiment_n33.Rdata'), verbose=TRUE)
@@ -11,5 +16,14 @@ load(here('raw-data/count_objects/rse_jx_Jlab_experiment_n33.Rdata'), verbose=TR
 
 ## Load sample data
 sample_data <- as.data.frame(read_excel("raw-data/FentanylvsSaline_SelfAdministration_RNAextraction.xlsx"))
+
+
+## Verify sample data is the same in gene, exon and jx datasets
+identical(colData(rse_gene), colData(rse_exon))
+# [1] TRUE
+identical(colData(rse_gene), colData(rse_jx))
+# [1] TRUE
+
+
 
 
