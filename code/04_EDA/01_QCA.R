71b3e8a5594875f324c0db493b4749ba1630e2e2
daianna21 <daianna2121@outlook.es>
2023-04-24 20:44:19 -0600

    Correct QC variables

--- code/04_EDA/01_QCA.R
+++ code/04_EDA/01_QCA.R
@@ -9,50 +9,20 @@ library(ggstatsplot)
 library(sessioninfo)
 
 
-#######################   Exploratory Data Analysis   #######################
+########################   Exploratory Data Analysis   #########################
 
 
-##  1. Quality Control Analysis
-## Note: genes were not filtered and counts were not normalized in these analyses
+################################################################################
+##                        1. Quality Control Analysis
+################################################################################
+## (Note: genes were not filtered and counts were not normalized in these analyses)
 
 ## Load RSE object at gene level
-load(here('raw-data/count_objects/rse_gene_Jlab_experiment_n33.Rdata'), verbose=TRUE)
-load(here('raw-data/count_objects/rse_exon_Jlab_experiment_n33.Rdata'), verbose=TRUE)
-load(here('raw-data/count_objects/rse_jx_Jlab_experiment_n33.Rdata'), verbose=TRUE)
-## Load sample data
-sample_data <- as.data.frame(read_excel("raw-data/Sample_data.xlsx"))
+load(here('processed-data/03_Data_preparation/rse_gene_sample_info.Rdata'), verbose=TRUE)
 
 
 
-## 1.1 Data exploration and preparation
-
-## Verify sample data is the same in gene, exon and jx datasets
-identical(colData(rse_gene), colData(rse_exon))
-# [1] TRUE
-identical(colData(rse_gene), colData(rse_jx))
-# [1] TRUE
-
-
-## Add sample data to colData of gene RSE
-
-## Revome last (empty) line and column
-sample_data <- sample_data[-nrow(sample_data),-ncol(sample_data)]
-## Correct colnames in sample data
-colnames(sample_data) <- str_replace_all(colnames(sample_data), c(" "="_"))
-## Correct sample ID in sample data
-sample_data$SAMPLE_ID <- str_replace_all(sample_data$Tissue_Punch_Label, c(" "="_", "-"="_"))
-## Correct Sample_No. for the extra sample
-sample_data[which(sample_data$SAMPLE_ID=="33_S_Amyg_20"), "Sample_No."]="33.0"
-
-## Verify they're the same samples
-setdiff(sample_data$SAMPLE_ID, rse_gene$SAMPLE_ID)
-# character(0)
-setdiff(rse_gene$SAMPLE_ID, sample_data$SAMPLE_ID)
-# character(0)
-
-## Merge data in colData
-colData(rse_gene) <- merge(colData(rse_gene), sample_data, by='SAMPLE_ID')
-
+## 1.1 QC variable construction and correction
 
 ## Add library size for each sample
 colData(rse_gene)$library_size <- apply(assay(rse_gene), 2, sum)
@@ -60,14 +30,22 @@ colData(rse_gene)$library_size <- apply(assay(rse_gene), 2, sum)
 ## Add detected number of genes (not zero-expressed genes) for each sample
 colData(rse_gene)$detected_num_genes <- apply(assay(rse_gene), 2, function(x){length(x[which(x>0)])})
 
+## Create a single and complete RIN column (add LIBD RIN if Psomagen is not available)
+colData(rse_gene)$RIN <- as.numeric(apply(colData(rse_gene), 1, function(x){if (x['Psomagen_RIN']!="error"){x['Psomagen_RIN']} else {x['RIN_LIBD']}}))
+
+## Correct data for RNA concentration
+colData(rse_gene)$RNA_concentration <- colData(rse_gene)$NanoDrop_or_BioA
+colData(rse_gene)$RNA_concentration <- as.vector(sapply(rse_gene$NanoDrop_or_BioA, function(x){if (strsplit(x, '')[[1]][1]=='*'){strsplit(x, '\\*')[[1]][2]} else {x}}))
+colData(rse_gene)$RNA_concentration <- as.numeric(colData(rse_gene)$RNA_concentration)
 
 
-## 1.2 Evaluate QC metrics of samples groups
 
-## Metrics of interest
-qc_metrics <- c('mitoRate', 'overallMapRate', 'totalAssignedGene', 'concordMapRate', 'library_size', 'detected_num_genes')
+## 1.2 Evaluate QC metrics for groups of samples
+
+## QC metrics of interest
+qc_metrics <- c('mitoRate', 'overallMapRate', 'totalAssignedGene', 'concordMapRate', 'library_size', 'detected_num_genes', 'RIN', 'RNA_concentration', 'Psomagen_Total_RNA_amount')
 ## Sample variables of interest
-sample_variables <- c("Brain_Region", "Substance")
+sample_variables <- c("Brain_Region", "Substance", "Num_Fentanyl_Sessions_six_hrs", 'Total_Num_Fentanyl_Sessions')
 
 
 ## Function to create boxplots of QC metrics for groups of samples
@@ -104,8 +82,8 @@ for (sample_var in sample_variables) {
         i=i+1
     }
     combine_plots(
-        list(plots[[1]], plots[[2]], plots[[3]], plots[[4]], plots[[5]], plots[[6]]),
-        plotgrid.args = list(nrow = 2)
+        list(plots[[1]], plots[[2]], plots[[3]], plots[[4]], plots[[5]], plots[[6]], plots[[7]], plots[[8]], plots[[9]]),
+        plotgrid.args = list(nrow = 3)
     )
     ggsave(paste("plots/01_EDA/01_QCA/QC_boxplots_", sample_var ,".pdf", sep=""), width=35, height=22, units = "cm")
 }
