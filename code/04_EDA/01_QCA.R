fdcdab426e5f87422fa171fc057a67f2e0c00de2
daianna21 <daianna2121@outlook.es>
2023-04-24 23:31:53 -0600

    Plots with ggplot

--- code/04_EDA/01_QCA.R
+++ code/04_EDA/01_QCA.R
@@ -5,7 +5,8 @@ library(readxl)
 library(stringr)
 library(ggplot2)
 library(rlang)
-library(ggstatsplot)
+library(smplot2)
+library(Hmisc)
 library(sessioninfo)
 
 
@@ -34,8 +35,7 @@ colData(rse_gene)$detected_num_genes <- apply(assay(rse_gene), 2, function(x){le
 colData(rse_gene)$RIN <- as.numeric(apply(colData(rse_gene), 1, function(x){if (x['Psomagen_RIN']!="error"){x['Psomagen_RIN']} else {x['RIN_LIBD']}}))
 
 ## Correct data for RNA concentration
-colData(rse_gene)$RNA_concentration <- colData(rse_gene)$NanoDrop_or_BioA
-colData(rse_gene)$RNA_concentration <- as.vector(sapply(rse_gene$NanoDrop_or_BioA, function(x){if (strsplit(x, '')[[1]][1]=='*'){strsplit(x, '\\*')[[1]][2]} else {x}}))
+colData(rse_gene)$RNA_concentration <- as.vector(sapply(rse_gene$RNA_concentration, function(x){if (strsplit(x, '')[[1]][1]=='*'){strsplit(x, '\\*')[[1]][2]} else {x}}))
 colData(rse_gene)$RNA_concentration <- as.numeric(colData(rse_gene)$RNA_concentration)
 
 
@@ -43,38 +43,61 @@ colData(rse_gene)$RNA_concentration <- as.numeric(colData(rse_gene)$RNA_concentr
 ## 1.2 Evaluate QC metrics for groups of samples
 
 ## QC metrics of interest
-qc_metrics <- c('mitoRate', 'overallMapRate', 'totalAssignedGene', 'concordMapRate', 'library_size', 'detected_num_genes', 'RIN', 'RNA_concentration', 'Psomagen_Total_RNA_amount')
+qc_metrics <- c('mitoRate', 'overallMapRate', 'totalAssignedGene', 'concordMapRate', 'library_size', 'detected_num_genes', 'RIN', 'RNA_concentration', 'Total_RNA_amount')
+
+## Create variable of Brain Region + Substance
+colData(rse_gene)$Brain_Region_and_Substance <- apply(colData(rse_gene), 1, function(x){ capitalize(paste(x['Brain_Region'], x['Substance'])) })
 ## Sample variables of interest
-sample_variables <- c("Brain_Region", "Substance", "Num_Fentanyl_Sessions_six_hrs", 'Total_Num_Fentanyl_Sessions')
+sample_variables <- c("Brain_Region", "Substance", "Brain_Region_and_Substance", "Num_Fentanyl_Sessions_six_hrs", 'Total_Num_Fentanyl_Sessions')
 
 
 ## Function to create boxplots of QC metrics for groups of samples
 
 QC_boxplots <- function(qc_metric, sample_var){
-    plot <- ggstatsplot::ggbetweenstats(
-        data = data.frame(colData(rse_gene)),
-        x = !! rlang::sym(sample_var),
-        y = !! rlang::sym(qc_metric),
-        mean.plotting = FALSE,
-        boxtype = "boxviolin",
-        xlab = str_replace_all(sample_var, c("_"=" ")),
-        ylab = str_replace_all(qc_metric, c("_"=" ")),
-        ## turn off messages
-        bf.message = FALSE,
-        results.subtitle = FALSE,
-        outlier.color = "red",
-        ggtheme = ggplot2::theme_gray(),
-        package = "yarrr", ## package for color palette
-        palette = "info2", ## color palette
-        point.args = list(alpha=0.7, size=2, position = ggplot2::position_jitterdodge(dodge.width = 0.6))
-    )
+
+    if (sample_var=="Brain_Region"){
+        colors=c('amygdala'='palegreen3', 'habenula'='orchid1')
+        violin_width=1
+        jitter_width=0.1
+    }
+
+    else if (sample_var=="Substance"){
+        colors=c('Fentanyl'='turquoise3', 'Saline'='yellow3')
+        violin_width=0.7
+        jitter_width=0.1
+    }
+
+    else if (sample_var=="Brain_Region_and_Substance"){
+        colors=c('Amygdala Fentanyl'='springgreen3' , 'Amygdala Saline'='yellowgreen', 'Habenula Fentanyl'='hotpink1', 'Habenula Saline'='violet')
+        violin_width=0.7
+        jitter_width=0.09
+    }
+
+    data <- data.frame(colData(rse_gene))
+    plot <- ggplot(data = data, mapping = aes(x = !! rlang::sym(sample_var), y = !! rlang::sym(qc_metric), color = !! rlang::sym(sample_var))) +
+                geom_violin(alpha = 0, size = 0.4, color='black', width=violin_width)+
+                geom_jitter(width = jitter_width, alpha = 0.7, size = 2) +
+                geom_boxplot(alpha = 0, size = 0.4, width=0.1, color='black') +
+                scale_color_manual(values = colors) +
+                sm_hgrid()
 
     return(plot)
 }
 
 
 ## Multiple plots
-for (sample_var in sample_variables) {
+
+for (sample_var in sample_variables){
+
+    if (sample_var=="Brain_Region_and_Substnace"){
+        width=50
+        height=40
+    }
+    else {
+        width=35
+        height=30
+    }
+
     i=1
     plots = list()
     for (qc_metric in qc_metrics) {
@@ -85,15 +108,9 @@ for (sample_var in sample_variables) {
         list(plots[[1]], plots[[2]], plots[[3]], plots[[4]], plots[[5]], plots[[6]], plots[[7]], plots[[8]], plots[[9]]),
         plotgrid.args = list(nrow = 3)
     )
-    ggsave(paste("plots/01_EDA/01_QCA/QC_boxplots_", sample_var ,".pdf", sep=""), width=35, height=22, units = "cm")
+    ggsave(paste("plots/01_EDA/01_QCA/QC_boxplots_", sample_var,".pdf", sep=""), width=35, height=22, units = "cm")
 }
 
-
-
-
-## Check dark dots
-
-## See https://mran.microsoft.com/snapshot/2018-05-29/web/packages/ggstatsplot/vignettes/ggbetweenstats.html
 
 
 
