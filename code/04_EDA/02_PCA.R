d7314088423081416f7033058104f6baf50aef77
daianna21 <daianna2121@outlook.es>
2023-05-10 23:34:21 -0600

    Add labels of sample outliers in PCA plots

--- code/04_EDA/02_PCA.R
+++ code/04_EDA/02_PCA.R
@@ -14,17 +14,25 @@ library(sessioninfo)
 ## (Note: genes are filtered and counts normalized in these analyses)
 
 load(here('processed-data/03_Data_preparation/rse_gene_filt.Rdata'), verbose=TRUE)
-load(here('processed-data/04_EDA/01_QCA/rse_gene_qc'))
-load(here('processed-data/04_EDA/01_QCA/rse_gene_amygdala_qc'))
+load(here('processed-data/04_EDA/01_QCA/rse_gene_qc'), verbose = TRUE)
+load(here('processed-data/04_EDA/01_QCA/rse_gene_amygdala_qc'), verbose = TRUE)
 load(here('processed-data/04_EDA/01_QCA/rse_gene_habenula_qc'), verbose = TRUE)
 
 ## Habenula filtered data
 rse_gene_habenula_filt <- rse_gene_filt[,colData(rse_gene_filt)$Brain_Region=='Habenula']
+save(rse_gene_habenula_filt, file='processed-data/04_EDA/02_PCA/rse_gene_habenula_filt.Rdata')
+## Amygdala filtered data
 rse_gene_amygdala_filt <- rse_gene_filt[,colData(rse_gene_filt)$Brain_Region=='Amygdala']
+save(rse_gene_amygdala_filt, file='processed-data/04_EDA/02_PCA/rse_gene_amygdala_filt.Rdata')
 
-### Explore samples' expression variation
+
+
+### Explore samples' gene expression variation
+
 
 ## 1.1 Generate PCA data
+
+## Function to obtain PC
 PCA<-function(brain_region){
     if (is.null(brain_region)){
         RSE <- rse_gene_filt
@@ -46,6 +54,9 @@ PCA<-function(brain_region){
 }
 
 
+
+## 1.2 Generate PCA plots
+
 ## PCx vs PCy Plots
 
 PCx_vs_PCy <- function (PCx, PCy, pca_data, pca_vars_labs, sample_var, brain_region) {
@@ -62,18 +73,20 @@ PCx_vs_PCy <- function (PCx, PCy, pca_data, pca_vars_labs, sample_var, brain_reg
 
     if(sample_var=='Brain_Region'){
         colors=c('Amygdala'='palegreen3', 'Habenula'='orchid1')
-        legend_height = 1.6
-        legend_width = 1.6
-        legend_text_size = 10
-        legend_title_size = 12
+        legend_height = 1
+        legend_width = 1
+        legend_text_size = 8
+        legend_title_size = 10
+        margin=1.5
     }
 
     else if(sample_var=='Substance'){
         colors=c('Fentanyl'='turquoise3', 'Saline'='yellow3')
-        legend_height = 1.6
-        legend_width = 1.6
-        legend_text_size = 10
-        legend_title_size = 12
+        legend_height = 1
+        legend_width = 1
+        legend_text_size = 8
+        legend_title_size = 10
+        margin=1.5
     }
 
     else if(sample_var=='Brain_Region_and_Substance'){
@@ -82,6 +95,7 @@ PCx_vs_PCy <- function (PCx, PCy, pca_data, pca_vars_labs, sample_var, brain_reg
         legend_width = 1
         legend_text_size = 8
         legend_title_size = 10
+        margin=0.3
     }
 
     else if(sample_var=='Total_Num_Fentanyl_Sessions'){
@@ -90,6 +104,7 @@ PCx_vs_PCy <- function (PCx, PCy, pca_data, pca_vars_labs, sample_var, brain_reg
         legend_width = 1
         legend_text_size = 8
         legend_title_size = 10
+        margin=0.2
     }
 
     else if(sample_var=='Num_Fentanyl_Sessions_six_hrs'){
@@ -98,6 +113,7 @@ PCx_vs_PCy <- function (PCx, PCy, pca_data, pca_vars_labs, sample_var, brain_reg
         legend_width = 1
         legend_text_size = 8
         legend_title_size = 10
+        margin=0.1
     }
 
     plot <- ggplot(data=pca_data,
@@ -105,7 +121,7 @@ PCx_vs_PCy <- function (PCx, PCy, pca_data, pca_vars_labs, sample_var, brain_reg
                 color=eval(parse_expr(sample_var)),
                 label=outlier_sample_labels) ) +
             theme_classic() +
-            theme(legend.position="right", plot.margin=unit (c (1,1,1,1), 'cm'),
+            theme(legend.position="right", plot.margin=unit (c (1,margin,1,margin), 'cm'),
                   legend.text = element_text(size=legend_text_size),
                   legend.title = element_text(size=legend_title_size)) +
             geom_point(size=2) +
@@ -143,7 +159,7 @@ plot_PCAs<-function(brain_region){
             plot_grid(plots[[1]], plots[[2]], plots[[3]], plots[[4]], plots[[5]], nrow = 2)
             ## Save plots
             ggsave(paste("plots/04_EDA/02_PCA/",PCs[1],"_vs_",PCs[2],"_all_samples.pdf", sep=""),
-                   width = 40, height = 20, units = "cm")
+                   width = 40, height = 19, units = "cm")
         }
     }
     ## For habenula and amygdala separately
@@ -151,13 +167,12 @@ plot_PCAs<-function(brain_region){
         for (PCs in list(c("PC1", "PC2"), c("PC3", "PC4"), c("PC5", "PC6"))){
             plots<-list()
             i=1
-            for (sample_var in c("Substance", "Brain_Region_and_Substance",
-                                 "Total_Num_Fentanyl_Sessions", "Num_Fentanyl_Sessions_six_hrs")){
+            for (sample_var in c("Substance", "Total_Num_Fentanyl_Sessions", "Num_Fentanyl_Sessions_six_hrs")){
                 p<-PCx_vs_PCy(PCs[1], PCs[2], pca_data, pca_vars_labs, sample_var, brain_region)
                 plots[[i]]=p
                 i=i+1
             }
-            plot_grid(plots[[1]], plots[[2]], plots[[3]], plots[[4]], nrow = 2)
+            plot_grid(plots[[1]], plots[[2]], plots[[3]], nrow = 2)
             ## Save plots
             ggsave(paste("plots/04_EDA/02_PCA/",PCs[1],"_vs_",PCs[2],"_", brain_region,".pdf", sep=""),
                    width = 28, height = 20, units = "cm")
@@ -166,5 +181,44 @@ plot_PCAs<-function(brain_region){
 }
 
 
+## Plots
+plot_PCAs(NULL)
+plot_PCAs('habenula')
+plot_PCAs('amygdala')
 
+
+
+## 1.3 Manual sample filtering
+
+## 1.3.1 Identify and label rare samples
+
+
+
+
+
+
+
+
+
+
+
+## Reproducibility information
+print('Reproducibility information:')
+Sys.time()
+proc.time()
+options(width = 120)
+session_info()
+
+# setting  value
+# version  R version 4.2.2 (2022-10-31)
+# os       macOS Monterey 12.5.1
+# system   aarch64, darwin20
+# ui       RStudio
+# language (EN)
+# collate  en_US.UTF-8
+# ctype    en_US.UTF-8
+# tz       America/Mexico_City
+# date     2023-05-10
+# rstudio  2022.12.0+353 Elsbeth Geranium (desktop)
+# pandoc   2.19.2 @ /private/var/folders/r6/94s0dsks4m3298b0d1mrp5tw0000gn/T/AppTranslocation/8639B884-1A6F-4EA4-9186-4C76049940ED/d/RStudio.app/Contents/Resources/app/quarto/bin/tools/ (via rmarkdown)
 
