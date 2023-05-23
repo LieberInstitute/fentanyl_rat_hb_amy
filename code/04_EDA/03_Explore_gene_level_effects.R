612e2e2118aa41e4f5dc278342fb2d360747ce9a
daianna21 <daianna2121@outlook.es>
2023-05-23 16:53:07 -0600

    Plot gene expression counts for most affected genes by each sample variable

--- code/04_EDA/03_Explore_gene_level_effects.R
+++ code/04_EDA/03_Explore_gene_level_effects.R
@@ -5,6 +5,7 @@ library(scater)
 library(rlang)
 library(smplot2)
 library(cowplot)
+library(Hmisc)
 library(sessioninfo)
 
 
@@ -67,6 +68,7 @@ expl_vars_amygdala <- as.data.frame(expl_var("amygdala"))
 plot_gene_expr <- function(brain_region, sample_var, gene_id){
 
    rse_gene <- eval(parse_expr(paste("rse_gene", brain_region, 'filt', sep="_")))
+   expl_vars <- eval(parse_expr(paste0('expl_vars_', brain_region)))
 
    if(sample_var=='Substance'){
         colors=c('Fentanyl'='turquoise3', 'Saline'='yellow3')
@@ -94,30 +96,51 @@ plot_gene_expr <- function(brain_region, sample_var, gene_id){
 
     ## Gene symbol
     gene_symbol <- rowData(rse_gene)[which(rowData(rse_gene)$ensemblID==gene_id), 'Symbol']
+    if (is.na(gene_symbol)){
+       gene_ids <- gene_id
+    }
+    else{
+       gene_ids <- paste(gene_symbol, gene_id, sep='-')
+    }
+
+    ## Percentage of variance explained by the variable
+    percentage <- signif(exp_vars[gene_id, sample_var], digits=3)
 
     ## Boxplots for discrete variables
     if (sample_var=='Substance' | sample_var=='Total_Num_Fentanyl_Sessions' | sample_var=='Batch_RNA_extraction' | sample_var=='Batch_lib_prep') {
         plot <- ggplot(data = as.data.frame(data), mapping = aes(x = !! rlang::sym(sample_var),
                                                                  y = gene_expr, color = !! rlang::sym(sample_var),
                                                                  color = !! rlang::sym(sample_var))) +
-            geom_boxplot(size = 0.4, width=0.4, color='black', outlier.color = "#FFFFFFFF") +
-            geom_jitter(width = 0.17, alpha = 1, size = 2) +
+            geom_boxplot(size = 0.25, width=0.32, color='black', outlier.color = "#FFFFFFFF") +
+            geom_jitter(width = 0.15, alpha = 1, size = 1) +
             scale_color_manual(values = colors) +
             sm_hgrid() +
-            labs(title = paste(gene_symbol, gene_id, sep='-'), y= 'lognorm counts', x = x_label) +
-            theme(axis.title = element_text(size = (9)),
-                  axis.text = element_text(size = (8)))
+            labs(title = gene_ids,
+                 subtitle = paste0("Variance explained: ", percentage, '%'),
+                 y= 'lognorm counts', x = x_label) +
+            theme(axis.title = element_text(size = (7)),
+                  axis.text = element_text(size = (6)),
+                  plot.title = element_text(hjust=0.5, size=7.5, face="bold"),
+                  plot.subtitle = element_text(size = 7, color='gray40'))
     }
 
     ## Scatterplots for continuous variables
     else {
         colors <- c('mitoRate'='khaki3', 'totalAssignedGene'='plum2', 'overallMapRate'='turquoise', 'concordMapRate'='lightsalmon',
-                    'detected_num_genes'='skyblue2', 'library_size'='palegreen3', 'RIN'='rosybrown3')
+                    'detected_num_genes'='skyblue2', 'library_size'='palegreen3', 'RIN'='rosybrown3', 'Total_RNA_amount'='brown4',
+                    'RNA_concentration'='blue3')
 
         plot <- ggplot(as.data.frame(data), aes(x=eval(parse_expr(sample_var)), y=gene_expr)) +
             geom_point(color=colors[sample_var], show.legend = FALSE, size=2) +
-            theme_classic() +
-            labs(x=sample_var, y='lognorm counts')
+            theme_classic(base_size = 5) +
+            labs(title = gene_ids,
+                 subtitle = paste0("Variance explained: ", percentage, '%'),
+                 y= 'lognorm counts', x = gsub('_', ' ', sample_var)) +
+            theme(plot.margin=unit (c (0.4,0.1,0.4,0.1), 'cm'),
+                  axis.title = element_text(size = (7)),
+                  axis.text = element_text(size = (6)),
+                  plot.title = element_text(hjust=0.5, size=7.5, face="bold"),
+                  plot.subtitle = element_text(size = 7, color='gray40'))
     }
 
     return(plot)
@@ -135,17 +158,23 @@ gene_expr_expl_var <- function(brain_region, sample_var){
     ## Plots of lognorm counts of those genes
     i=1
     plots=list()
-    for (gene_id in top_genes){
-        plots[[1]] = plot_gene_expr(brain_region, sample_var, gene_id)
+    for (gene_id in rownames(top_genes_expl_vars)){
+        plots[[i]] = plot_gene_expr(brain_region, sample_var, gene_id)
         i=i+1
     }
 
     plot_grid(plots[[1]], plots[[2]], plots[[3]], plots[[4]], plots[[5]], plots[[6]], nrow=2)
-    ggsave(here(paste("plots/04_EDA/03_Explore_gene_level_effects/", var1,".pdf", sep="")), width = 40, height = 30, units = "cm")
+    ggsave(here(paste("plots/04_EDA/03_Explore_gene_level_effects/01_Expl_vars/Top_affected_genes_by_", capitalize(sample_var), "_", brain_region, ".pdf", sep="")), width = 18, height = 13, units = "cm")
 }
 
 
-
+## Plots
+gene_expr_expl_var('habenula', 'Substance')
+gene_expr_expl_var('habenula', 'library_size')
+gene_expr_expl_var('habenula', 'totalAssignedGene')
+gene_expr_expl_var('habenula', 'Batch_RNA_extraction')
+gene_expr_expl_var('habenula', 'RNA_concentration')
+gene_expr_expl_var('habenula', 'Total_RNA_amount')
 
 
 
