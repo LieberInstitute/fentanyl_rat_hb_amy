8d3c38d6f66f0fccd9d709b3c9ced4ec5b175d8e
daianna21 <daianna2121@outlook.es>
2024-04-23 21:46:22 -0600

    Re-run GO and KEGG ORAs

--- code/06_GO_KEGG/01_GO_KEGG_Analyses.R
+++ code/06_GO_KEGG/01_GO_KEGG_Analyses.R
@@ -8,37 +8,37 @@ library(sessioninfo)
 
 #######################  Functional Enrichment Analysis  #######################
 
-load(here('processed-data/05_DEA/results_all_vars_habenula.Rdata'))
-load(here('processed-data/05_DEA/results_uncorr_vars_habenula.Rdata'))
-load(here('processed-data/05_DEA/results_all_vars_amygdala.Rdata'))
-load(here('processed-data/05_DEA/results_uncorr_vars_amygdala.Rdata'))
+load(here('processed-data/05_DEA/de_genes_Substance_habenula.Rdata'), verbose = TRUE)
+load(here('processed-data/05_DEA/de_genes_Substance_amygdala.Rdata'), verbose = TRUE)
+load(here('processed-data/05_DEA/results_Substance_uncorr_vars_amygdala.Rdata'), verbose = TRUE)
 
 
 ## Groups of DEGs
 
-## Habenula DEGs from model with uncorrelated sample variables
-top_genes_hab <- results_uncorr_vars_habenula[[1]]
-## All DEGs
-DEGs_hab <- top_genes_hab[which(top_genes_hab$adj.P.Val<0.05), ]
-## Up and down DEGs
-up_hab <- top_genes_hab[which(top_genes_hab$adj.P.Val<0.05 & top_genes_hab$logFC>0), ]
-down_hab <- top_genes_hab[which(top_genes_hab$adj.P.Val<0.05 & top_genes_hab$logFC<0), ]
+########################
+##    Habenula DEGs
+########################
+## Up and down habenula DEGs from model with uncorrelated sample variables
+up_hab <- de_genes_habenula[which(de_genes_habenula$logFC>0), ]
+down_hab <- de_genes_habenula[which(de_genes_habenula$logFC<0), ]
 
-## Amygdala DEGs from model with uncorrelated sample variables
-top_genes_amy <- results_uncorr_vars_amygdala[[1]]
-DEGs_amy <- top_genes_amy[which(top_genes_amy$adj.P.Val<0.05), ]
-up_amy <- top_genes_amy[which(top_genes_amy$adj.P.Val<0.05 & top_genes_amy$logFC>0), ]
-down_amy <- top_genes_amy[which(top_genes_amy$adj.P.Val<0.05 & top_genes_amy$logFC<0), ]
+########################
+##    Amygdala DEGs
+########################
+## Up and down amygdala DEGs from model with uncorrelated sample variables
+up_amy <- de_genes_amygdala[which(de_genes_amygdala$logFC>0), ]
+down_amy <- de_genes_amygdala[which(de_genes_amygdala$logFC<0), ]
 
 
-## Function to do GO and KEGG analyses
+
+## Function to find enriched GO and KEGG terms
 
 GO_KEGG<- function(sigGeneList, geneUniverse, name){
 
-    height=10
-    width=9
+    height=5
+    width=7
 
-    ## Do GO
+    ## GO terms
     ## Obtain biological processes
     goBP_Adj <- compareCluster(
         sigGeneList,
@@ -71,7 +71,6 @@ GO_KEGG<- function(sigGeneList, geneUniverse, name){
         readable = TRUE
     )
 
-    ## Save
     if (!is.null(goMF_Adj)){
         pdf(paste("plots/06_GO_KEGG/GO_MF_", name, ".pdf", sep=""), height = height, width = width)
         print(dotplot(goMF_Adj, title="GO Enrichment Analysis: Molecular function"))
@@ -91,7 +90,6 @@ GO_KEGG<- function(sigGeneList, geneUniverse, name){
         readable = TRUE
     )
 
-    ## Save
     if (!is.null(goCC_Adj)){
         pdf(paste("plots/06_GO_KEGG/GO_CC_", name, ".pdf", sep=""), height = height, width = width)
         print(dotplot(goCC_Adj, title="GO Enrichment Analysis: Cellular components"))
@@ -99,7 +97,7 @@ GO_KEGG<- function(sigGeneList, geneUniverse, name){
     }
 
 
-    ## Do KEGG
+    ## KEGG terms
     kegg_Adj <- compareCluster(
         sigGeneList,
         fun = "enrichKEGG",
@@ -109,7 +107,6 @@ GO_KEGG<- function(sigGeneList, geneUniverse, name){
         qvalueCutoff = 0.05
     )
 
-    ## Save
     if (!is.null(kegg_Adj)){
         pdf(paste("plots/06_GO_KEGG/KEGG_", name, ".pdf", sep=""), height = height, width = width)
         print(dotplot(kegg_Adj, title="KEGG Enrichment Analysis"))
@@ -128,16 +125,16 @@ GO_KEGG<- function(sigGeneList, geneUniverse, name){
 }
 
 
-##################################  All DEGs  ##################################
+## 1. Analysis for all DEGs from each brain region
 
 ######################
 #      Habenula
 ######################
 
 ## List of DEGs
-sigGeneList <- list("All"= DEGs_hab[which(!is.na(DEGs_hab$EntrezID) & !DEGs_hab$EntrezID=='NULL'), 'EntrezID'])
-## Background genes
-geneUniverse <- as.character(top_genes_hab$EntrezID)
+sigGeneList <- list("All"= de_genes_habenula[which(!is.na(de_genes_habenula$EntrezID) & !de_genes_habenula$EntrezID=='NULL'), 'EntrezID'])
+## Background genes (all genes assessed for DGE)
+geneUniverse <- as.character(results_Substance_uncorr_vars_amygdala[[1]]$EntrezID)
 geneUniverse <- geneUniverse[!is.na(geneUniverse) & !geneUniverse=='NULL']
 
 goList_habenula_all_DEGs<-GO_KEGG(sigGeneList, geneUniverse, 'habenula_all_DEGs')
@@ -147,16 +144,16 @@ save(goList_habenula_all_DEGs, file="processed-data/06_GO_KEGG/goList_habenula_a
 #      Amygdala
 ######################
 
-sigGeneList <- list("All"= DEGs_amy[which(!is.na(DEGs_amy$EntrezID) & !DEGs_amy$EntrezID=='NULL'), 'EntrezID'])
-geneUniverse <- as.character(top_genes_amy$EntrezID)
-geneUniverse <- geneUniverse[!is.na(geneUniverse) & !geneUniverse=='NULL']
+sigGeneList <- list("All"= de_genes_amygdala[which(!is.na(de_genes_amygdala$EntrezID) & !de_genes_amygdala$EntrezID=='NULL'), 'EntrezID'])
 
 goList_amygdala_all_DEGs<-GO_KEGG(sigGeneList, geneUniverse, 'amygdala_all_DEGs')
 save(goList_amygdala_all_DEGs, file="processed-data/06_GO_KEGG/goList_amygdala_all_DEGs.Rdata")
 
 
 
-################################  Up/Down DEGs  ################################
+
+
+## 2. Analysis for up- and down-regulated DEGs from each brain region
 
 ######################
 #      Habenula
@@ -165,13 +162,9 @@ save(goList_amygdala_all_DEGs, file="processed-data/06_GO_KEGG/goList_amygdala_a
 ## List of DEG sets
 sigGeneList <- list("Up"=up_hab[which(!is.na(up_hab$EntrezID) & !up_hab$EntrezID=='NULL'), 'EntrezID'],
                     "Down"=down_hab[which(!is.na(down_hab$EntrezID) & !down_hab$EntrezID=='NULL'), 'EntrezID'])
-## Background genes
-geneUniverse <- as.character(top_genes_hab$EntrezID)
-geneUniverse <- geneUniverse[!is.na(geneUniverse) & !geneUniverse=='NULL']
 
 goList_habenula_up_down_DEGs<-GO_KEGG(sigGeneList, geneUniverse, 'habenula_up_down_DEGs')
 save(goList_habenula_up_down_DEGs, file="processed-data/06_GO_KEGG/goList_habenula_up_down_DEGs.Rdata")
-
 
 ######################
 #      Amygdala
@@ -180,10 +173,17 @@ save(goList_habenula_up_down_DEGs, file="processed-data/06_GO_KEGG/goList_habenu
 sigGeneList <- list("Up"=up_amy[which(!is.na(up_amy$EntrezID) & !up_amy$EntrezID=='NULL'), 'EntrezID'],
                     "Down"=down_amy[which(!is.na(down_amy$EntrezID) & !down_amy$EntrezID=='NULL'), 'EntrezID'])
 
-geneUniverse <- as.character(top_genes_amy$EntrezID)
-geneUniverse <- geneUniverse[!is.na(geneUniverse) & !geneUniverse=='NULL']
-
 goList_amygdala_up_down_DEGs<-GO_KEGG(sigGeneList, geneUniverse, 'amygdala_up_down_DEGs')
 save(goList_amygdala_up_down_DEGs, file="processed-data/06_GO_KEGG/goList_amygdala_up_down_DEGs.Rdata")
 
+
+
+
+
+
+
+## Reproducibility information
+
+options(width = 120)
+session_info()
 
