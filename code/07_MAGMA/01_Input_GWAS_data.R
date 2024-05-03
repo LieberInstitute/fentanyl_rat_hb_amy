a15dccd2acee935b730215069611bb95ee087fec
daianna21 <daianna2121@outlook.es>
2024-05-02 20:59:41 -0400

    Add session info and remove old scripts

--- code/07_MAGMA/01_Input_GWAS_data.R
+++ code/07_MAGMA/01_Input_GWAS_data.R
@@ -568,3 +568,71 @@ snp_pval_OUD <- as.data.frame(snp_pval_OUD)
 ## Save
 save(snp_pval_OUD, file=here('processed-data/07_MAGMA/Input_GWAS_data/OUD/OUD.phs001672.pha004954.pval'))
 
+
+
+
+
+
+
+## Reproducibility information
+
+options(width = 120)
+session_info()
+#                    4.0.5     2022-11-15 [2] CRAN (R 4.3.1)
+# bit64              4.0.5     2020-08-30 [2] CRAN (R 4.3.1)
+# bitops             1.0-7     2021-04-24 [2] CRAN (R 4.3.1)
+# blob               1.2.4     2023-03-17 [2] CRAN (R 4.3.1)
+# cachem             1.0.8     2023-05-01 [2] CRAN (R 4.3.1)
+# cli                3.6.1     2023-03-23 [2] CRAN (R 4.3.1)
+# crayon             1.5.2     2022-09-29 [2] CRAN (R 4.3.1)
+# curl               5.0.2     2023-08-14 [2] CRAN (R 4.3.1)
+# data.table       * 1.14.8    2023-02-17 [2] CRAN (R 4.3.1)
+# DBI                1.1.3     2022-06-18 [2] CRAN (R 4.3.1)
+# dbplyr             2.3.3     2023-07-07 [2] CRAN (R 4.3.1)
+# digest             0.6.33    2023-07-07 [2] CRAN (R 4.3.1)
+# dplyr              1.1.3     2023-09-03 [2] CRAN (R 4.3.1)
+# fansi              1.0.4     2023-01-22 [2] CRAN (R 4.3.1)
+# fastmap            1.1.1     2023-02-24 [2] CRAN (R 4.3.1)
+# filelock           1.0.2     2018-10-05 [2] CRAN (R 4.3.1)
+# generics           0.1.3     2022-07-05 [2] CRAN (R 4.3.1)
+# GenomeInfoDb       1.36.3    2023-09-07 [2] Bioconductor
+# GenomeInfoDbData   1.2.10    2023-07-20 [2] Bioconductor
+# glue               1.6.2     2022-02-24 [2] CRAN (R 4.3.1)
+# here             * 1.0.1     2020-12-13 [2] CRAN (R 4.3.1)
+# hms                1.1.3     2023-03-21 [2] CRAN (R 4.3.1)
+# httr               1.4.7     2023-08-15 [2] CRAN (R 4.3.1)
+# IRanges          * 2.34.1    2023-06-22 [2] Bioconductor
+# KEGGREST           1.40.0    2023-04-25 [2] Bioconductor
+# lifecycle          1.0.3     2022-10-07 [2] CRAN (R 4.3.1)
+# magrittr           2.0.3     2022-03-30 [2] CRAN (R 4.3.1)
+# memoise            2.0.1     2021-11-26 [2] CRAN (R 4.3.1)
+# org.Hs.eg.db     * 3.17.0    2023-07-20 [2] Bioconductor
+# pillar             1.9.0     2023-03-22 [2] CRAN (R 4.3.1)
+# pkgconfig          2.0.3     2019-09-22 [2] CRAN (R 4.3.1)
+# png                0.1-8     2022-11-29 [2] CRAN (R 4.3.1)
+# prettyunits        1.1.1     2020-01-24 [2] CRAN (R 4.3.1)
+# progress           1.2.2     2019-05-16 [2] CRAN (R 4.3.1)
+# R6                 2.5.1     2021-08-19 [2] CRAN (R 4.3.1)
+# rappdirs           0.3.3     2021-01-31 [2] CRAN (R 4.3.1)
+# RCurl              1.98-1.12 2023-03-27 [2] CRAN (R 4.3.1)
+# rlang              1.1.1     2023-04-28 [2] CRAN (R 4.3.1)
+# rprojroot          2.0.3     2022-04-02 [2] CRAN (R 4.3.1)
+# RSQLite            2.3.1     2023-04-03 [2] CRAN (R 4.3.1)
+# S4Vectors        * 0.38.1    2023-05-02 [2] Bioconductor
+# sessioninfo      * 1.2.2     2021-12-06 [2] CRAN (R 4.3.1)
+# stringi            1.7.12    2023-01-11 [2] CRAN (R 4.3.1)
+# stringr            1.5.0     2022-12-02 [2] CRAN (R 4.3.1)
+# tibble             3.2.1     2023-03-20 [2] CRAN (R 4.3.1)
+# tidyselect         1.2.0     2022-10-10 [2] CRAN (R 4.3.1)
+# utf8               1.2.3     2023-01-31 [2] CRAN (R 4.3.1)
+# vctrs              0.6.3     2023-06-14 [2] CRAN (R 4.3.1)
+# XML                3.99-0.14 2023-03-19 [2] CRAN (R 4.3.1)
+# xml2               1.3.5     2023-07-06 [2] CRAN (R 4.3.1)
+# XVector            0.40.0    2023-04-25 [2] Bioconductor
+# zlibbioc           1.46.0    2023-04-25 [2] Bioconductor
+#
+# [1] /users/dgonzale/R/4.3
+# [2] /jhpce/shared/community/core/conda_R/4.3/R/lib64/R/site-library
+# [3] /jhpce/shared/community/core/conda_R/4.3/R/lib64/R/library
+#
+# ─────────────────────────────────────────────────────────────────────────────────────────────────────────────────────────────────────────────────────────────────
